986c3806bc7a65a60ce3996eff6983e86e1c46af
Alexander Mikholap <alxandr.mikholap@gmail.com>
2026-04-29 16:57:13 +0300

    Add auth page and sign-in form

--- src/app/(auth)/auth/page.tsx
+++ src/app/(auth)/auth/page.tsx
@@ -1,6 +1,34 @@
-export default function AuthPage() {
-    
-    return (
-        <div>Auth page</div>
-    )
+import SignInForm from 'modules/sign-in-form';
+import { Surface, Tabs } from '@heroui/react';
+
+export default async function AuthPage() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-background">
+      <Surface
+        className="flex min-w-[320px] w-xl flex-col gap-3 rounded-3xl p-6"
+        variant="secondary"
+      >
+        <Tabs className="w-full">
+          <Tabs.ListContainer>
+            <Tabs.List aria-label="Options">
+              <Tabs.Tab id="sign-in">
+                Sing In
+                <Tabs.Indicator />
+              </Tabs.Tab>
+              <Tabs.Tab id="sign-up">
+                Sing Up
+                <Tabs.Indicator />
+              </Tabs.Tab>
+            </Tabs.List>
+          </Tabs.ListContainer>
+          <Tabs.Panel className="pt-4" id="sign-in">
+            <SignInForm />
+          </Tabs.Panel>
+          <Tabs.Panel className="pt-4" id="sign-up">
+            <p>Track your metrics and analyze performance data.</p>
+          </Tabs.Panel>
+        </Tabs>
+      </Surface>
+    </div>
+  );
 }
