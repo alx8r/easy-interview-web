220e8dc1dec8575f3df3ebc3518db6229adfac7d
Alexander Mikholap <alxandr.mikholap@gmail.com>
2026-04-29 13:36:31 +0300

    Update eslint, prettier and husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,54 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+
+  {
+    rules: {
+      /**
+       * Общие правила качества кода
+       */
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-debugger': 'warn',
+
+      /**
+       * TypeScript
+       */
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+
+      /**
+       * React / Next.js
+       */
+      'react/jsx-key': 'error',
+      'react/self-closing-comp': 'error',
+
+      /**
+       * Imports
+       */
+      'sort-imports': [
+        'warn',
+        {
+          ignoreDeclarationSort: true,
+          ignoreCase: true,
+        },
+      ],
+    },
+  },
+
+  eslintConfigPrettier,
+
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'next-env.d.ts']),
 ]);
 
 export default eslintConfig;
